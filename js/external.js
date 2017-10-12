d1b59f89f4277a22df72d096de248ad21fa4b48f
nicholekirlin <nichole.kirlin@gmail.com>
2017-10-12 10:41:56 -0500

    beginning of javascript work

---
+++ js/external.js
@@ -0,0 +1,9 @@
+"use strict"
+
+console.log("Hello from JavaScript!");
+
+alert("Welcome to my Website!");
+
+var userInput = prompt("What is your favorite color?");
+alert('Great, ' + userInput + " is my favorite color too!");
+
