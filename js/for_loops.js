0668476f2df665c09ff3bd711c49041529f06030
nicholekirlin <nichole.kirlin@gmail.com>
2017-10-17 16:00:17 -0500

    javascript loop exercises - multiple

--- js/for_loops.js
+++ js/for_loops.js
@@ -2,15 +2,16 @@
 
 //#1 multiplication table done
 
-//
-// function showMultiplicationTable(number){
-//     for (var i = 1; i <= 10; i++) {
-//         console.log(number + " x " + i + " = " + number * i);
-//     }
-// }
-//
-// showMultiplicationTable(7);
-//
+
+function showMultiplicationTable(number){
+    for (var i = 1; i <= 10; i++) {
+        console.log(number + " x " + i + " = " + number * i);
+    }
+}
+
+showMultiplicationTable(7);
+showMultiplicationTable(8);
+showMultiplicationTable(9);
 
 
 //#2 20-200 done
@@ -18,7 +19,7 @@
 
 for (var i = 1; i <= 10; i++) {
 
-    var random = Math.floor((Math.random()*200)+20);
+    var random = Math.floor((Math.random()*180)+20);
 
     if (i % 2 === 0) {
         console.log(random + " is even");
@@ -31,19 +32,24 @@ for (var i = 1; i <= 10; i++) {
 //#3 diamond done
 
 
-for (var i = 0; i <= 9; i++){
+// for (var i = 0; i <= 9; i++){
+//
+//     var a = "";
+//     // console.log("");
+//
+//     for (var j = 0; j < i; j++){
+//
+//         var a = a + "" + i
+//         // console.log();
+//     }
+//
+//     console.log(a)
+// }
 
-    var a = "";
-    // console.log("");
 
-    for (var j = 0; j < i; j++){
+//* diamond*
 
-        var a = a + "" + i
-        // console.log();
-    }
 
-    console.log(a)
-}
 
 
 // #4 100 countdown done
--- js/while.js
+++ js/while.js
@@ -1,40 +1,45 @@
 "use strict"
 
 // #1 done
-// var i = 1
-//
-// while (i < 65536) {
-//     i *= 2
-//     console.log(i);
-// }
+var i = 1
 
+while (i < 65536) {
+    i *= 2
+    console.log(i);
+}
+
+
+//Ryan's exercise
+var number = 2
+
+do {
+    console.log(number);
+
+    number = number * 2;
+
+} while(number <= 65536);
 
 
 //#2 ice cream
 
-// var allCones = Math.floor(Math.random() * 50) + 50;
-// Math.floor(Math.random() * 5) + 1
-//
-// var i = 2
-//
-// do {
-//     console.log("Ice cream iteration " + i);
-//     i++;
-// } while (i <= 100);
-//
-//
+var allCones = Math.floor(Math.random() * 50) + 50;
 
+do {
+    var conesOrdered = Math.floor(Math.random() * 5) + 1;
 
-
-
-for (var i = 1; i <= 5; i++) {
-
-    var random = Math.floor((Math.random()*5)+1);
-
-    if (i % 2 === 0) {
-        console.log(random + " ... I only have 3 more cones");
+    if(conesOrdered <= allCones) {
+        console.log("The customer ordered " + conesOrdered + " number of cones.");
+        allCones = allCones - conesOrdered;
+        console.log("I sold " + conesOrdered + " number of cones.");
+        console.log("My inventory is " + allCones);
+    } else {
+        console.log("Next customer, please");
     }
-    else {
-        console.log(random + " ... order up!");
-    }
+} while(allCones > 0);
+
+
+// Fizz Buzz
+for (var i = 1; i <= 100; i++) {
+    var f = i % 3 == 0, b = i % 5 == 0;
+    console.log(f ? b ? "FizzBuzz" : "Fizz" : b ? "Buzz" : i);
 }
