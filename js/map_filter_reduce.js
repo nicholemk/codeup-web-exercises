e92c24d0509285c51d57248789c2d07c8fb7a000
nicholekirlin <nichole.kirlin@gmail.com>
2017-11-08 16:57:32 -0600

    added map, filter, reduce exercise

---
+++ js/map_filter_reduce.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+    },
+];
+
+const evens = numbers.filter(function(languages) {
+    return (languages % 2 == 0) ? true : false;
+});
+console.log(evens);
+
+const namesToEmail = email.map(function(user) {
+    return namesToEmail;
+});
+console.log(email);
