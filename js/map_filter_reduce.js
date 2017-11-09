569a461529907669bdad3d0f6cf3fdc0a7128489
nicholekirlin <nichole.kirlin@gmail.com>
2017-11-09 15:26:08 -0600

    added map, filter, reduce exercises

--- js/map_filter_reduce.js
+++ js/map_filter_reduce.js
@@ -32,17 +32,53 @@ const users = [
         languages: ['html', 'css', 'javascript', 'php'],
     },
 ];
+    //
+    // const evens = findLanguages.filter(function(id) {
+    //     return (languages >= 3);
+    // });
+    // console.log(evens);
+    //
+    //
+    //
+    // const namesToEmail = email.map(function(user) {
+    //     return namesToEmail;
+    // });
+    // console.log(email);
+    //
+    //
+    //
+    // const findKey = users.reduce((id, name, email, languages) => {
+    //     return (array);
+    // });
+    //
 
-const evens = numbers.filter(function(languages) {
-    return (languages % 2 == 0) ? true : false;
-});
-console.log(evens);
+
+// Use .filter to create an array of user objects where each user object has at least 3 languages in the languages array.
+
+        const filteredUsers = users.filter((element) => {
+            return element.languages.length >= 3;
+        });
+
+        console.log(filteredUsers);
+
+//     Use .map to create an array of strings where each element is a user's email address
+
+        let arrayOfEmails = [];
+
+        users.forEach((element) => {
+            arrayOfEmails.push(element.email);
+        })
+
+        console.log(arrayOfEmails);
 
 
-const namesToEmail = email.map(function(user) {
-    return namesToEmail;
-});
-console.log(email);
+// Use .reduce to transform the array into an object where the object's keys are ids and the values are objects that represent each user
 
+        const usersObject = users.reduce((object, user) => {
+            object["id"] = user;
+            return object;
 
-const sum =
+            }, {});
+
+        console.log(usersObject);
+
